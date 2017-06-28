df564234c96ae08a51cc7249e2dbec769f69fd91
finnporter <iamfinnporter@gmail.com>
2017-06-28 16:44:13 +0100

    creates QuadBike.java with properties

---
+++ java_driver/car_management/QuadBike.java
@@ -0,0 +1,12 @@
+package car_management;
+import behaviours.*;
+
+public class QuadBike implements Drivable {
+
+  int averageSpeed;
+  int engineCapacity;
+
+  public QuadBike(int averageSpeed, int engineCapacity);
+    this.averageSpeed = averageSpeed;
+    this.engineCapacity = engineCapacity;
+}
